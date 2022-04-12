cd7492ff084e7b031d4fd36d6b8421254c91d198
Dieison Alessi <dieison.alessi@gmail.com>
2022-04-12 20:48:05 -0300

    Entrega S3-10

---
+++ script.js
@@ -0,0 +1,23 @@
+let nomesCompetidores = ["Daniel", "Rafael", "Manoel"]
+let colocacao = [1, 2, 3]
+function primeiroLugar(nomesCompetidores, colocacao) {
+    if (nomesCompetidores, colocacao) {
+        nomesCompetidores[0] == "Daniel" && colocacao[0] == 1
+        return colocacao[0] + " " + nomesCompetidores[0]
+    }
+}
+console.log(primeiroLugar(nomesCompetidores, colocacao))
+function segundoLugar(nomesCompetidores, colocacao) {
+    if (nomesCompetidores, colocacao) {
+        nomesCompetidores[1] == "Rafael" && colocacao[1] == 2
+        return colocacao[1] + " " + nomesCompetidores[1]
+    }
+}
+console.log(segundoLugar(nomesCompetidores, colocacao))
+function terceiroLugar(nomesCompetidores, colocacao) {
+    if (nomesCompetidores, colocacao) {
+        nomesCompetidores[2] == "Manoel" && colocacao[2] == 3
+        return colocacao[2] + " " + nomesCompetidores[2]
+    }
+}
+console.log(terceiroLugar(nomesCompetidores, colocacao))
