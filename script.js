e48e01dcd1a4bf932f9c997979f1d940c62b9b56
dieison-a <103083947+dieison-a@users.noreply.github.com>
2022-04-18 12:28:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let nomesCompetidores = ["Daniel", "Rafael", "Manoel"]
-let colocacao = [1, 2, 3]
-function primeiroLugar(nomesCompetidores, colocacao) {
-    if (nomesCompetidores, colocacao) {
-        nomesCompetidores[0] == "Daniel" && colocacao[0] == 1
-        return colocacao[0] + " " + nomesCompetidores[0]
+// fazer o ranekamento do 1º 2º e 3º -> PARÂMETRO
+// declarar variável de 1º 2º e 3º -> VARIÁVEL
+// se for competidor índice 0 -> MANTÉM
+// se for competidor índice 1 -> 1 VAI PRA 0 E 0 É "DANIEL" 
+// se for competidor índice 2 -> 1 VAI PRA 1 E 1 É "DANIEL"
+// mostrar no console o resultado da variável, dentro da função -> CONSOLE.LOG(VARIÁVEL)
+// fora, chamar a função e declarar os valores -> "Fulano" "Cicrano" "Beltrano"
+
+function ranking(primeiro, segundo, terceiro) {
+    let competidores = [primeiro, segundo, terceiro]
+    if (competidores[0] === "Daniel") {
+        competidores[0] === "Daniel"
     }
-}
-console.log(primeiroLugar(nomesCompetidores, colocacao))
-function segundoLugar(nomesCompetidores, colocacao) {
-    if (nomesCompetidores, colocacao) {
-        nomesCompetidores[1] == "Rafael" && colocacao[1] == 2
-        return colocacao[1] + " " + nomesCompetidores[1]
+    if (competidores[1] === "Daniel") {
+        competidores[1] = competidores[0]
+        competidores[0] = "Daniel"
     }
-}
-console.log(segundoLugar(nomesCompetidores, colocacao))
-function terceiroLugar(nomesCompetidores, colocacao) {
-    if (nomesCompetidores, colocacao) {
-        nomesCompetidores[2] == "Manoel" && colocacao[2] == 3
-        return colocacao[2] + " " + nomesCompetidores[2]
+    if (competidores[2] === "Daniel") {
+        competidores[2] = competidores[1]
+        competidores[1] = "Daniel"
     }
+    console.log(competidores)
 }
-console.log(terceiroLugar(nomesCompetidores, colocacao))
+ranking("Rafael", "Manoel", "Daniel")
